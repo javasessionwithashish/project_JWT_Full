587374e3b0eb052e9d40173d128ff6cc0af73afa
javasessionwithashish <ashish.koirala004@gmail.com>
2026-08-03 09:08:02 +0545

    2ndcommit

--- src/main/java/com/example/demo/Configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/Configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.example.demo.component;
+package com.example.demo.Configuration;
 
+import com.example.demo.component.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,16 +1,38 @@
 package com.example.demo.controller;
 
+import com.example.demo.component.JWUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
+	@Autowired
+	private JWUtil jwutil;
 
 	
 	@GetMapping("/")
 	public String login()
 	{
 		return  "index.html";
+	}
+
+
+	@PostMapping("/login")
+	public String loginPost(HttpServletRequest request)
+	{
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+
+
+
+		return "home";
 	}
 	
 }
